a285522a1a2b2fd8b91f826d11e4f398adb0c0e9
wandji20 <wbertrand10@gmail.com>
2021-10-24 05:06:35 +0100

    Add Funtions for displaying and hiding nav sub items

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 import NavMenu from './NavMenu';
 
-const Nav = () => (
-  <nav id="" className="nav-bar bg-gray-500 fixed-inset-0 flex justify-between items-center">
-    <NavLink to="#" className="block h-auto pl-2">
-      <span className="">
-        cars-4-all
-      </span>
-    </NavLink>
-    <NavMenu />
-    <button type="button" className="h-auto pr-2">
-      <FontAwesomeIcon icon={faBars} />
-    </button>
-  </nav>
-);
+const Nav = () => {
+  const [toggle, setToggle] = useState(true);
+  const handleToggleNav = () => {
+    setToggle((toggle) => !toggle);
+  };
+  return (
+    <nav id="" className="nav-bar bg-gray-500 fixed-inset-0 flex justify-between items-center">
+      <NavLink to="#" className="block h-auto pl-2">
+        <span className="">
+          cars-4-all
+        </span>
+      </NavLink>
+      <NavMenu toggle={toggle} handleToggleNav={handleToggleNav} />
+      <button
+        type="button"
+        className="h-auto pr-2"
+        onClick={handleToggleNav}
+      >
+        {toggle && <FontAwesomeIcon icon={faBars} />}
+        {!toggle && <FontAwesomeIcon icon={faTimes} />}
+      </button>
+    </nav>
+  );
+};
 
 export default Nav;
--- src/components/nav/NavMenu.js
+++ src/components/nav/NavMenu.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
-const NavMenu = () => {
+const NavMenu = (props) => {
+  const { toggle, handleToggleNav } = props;
   const pages = ['rental', 'market'];
-  const featuredCars = ['', 'Sedan', 'Suv', 'Vans', 'Truck'];
+  const featuredCars = ['', 'Sedan', 'Suv & Crossover', 'Van & Bus', 'Truck'];
+  const loggedIn = false;
+
+  const [togglePages, setTogglePages] = useState(false);
+  const [toggleFeaturedCars, setToggleFeaturedCars] = useState(false);
+  const [toggleProfile, setToggleProfile] = useState(false);
+
+  const resetToggles = () => {
+    setToggleFeaturedCars(false);
+    setTogglePages(false);
+    setToggleProfile(false);
+  };
 
   return (
     <div
-      className="w-1/2 flex flex-col items-start justify-around absolute nav-menu bg-gray-100 px-4 bg-gray hidden"
+      className={toggle ? 'hidden' : 'w-1/2 flex flex-col items-start justify-around absolute nav-menu bg-gray-100 px-4 bg-gray h-auto'}
     >
       <div className="h-auto border-black w-full">
-        <span className="inline-block">Pages</span>
-        <span className="inline-block ml-2">
-          <FontAwesomeIcon icon={faCaretDown} />
-        </span>
-        <ul id="pages" className="w-full hidden">
+        <button className="inline" type="button" onClick={() => setTogglePages((prev) => !prev)}>
+          <span className="inline-block">Pages</span>
+          <span className="inline-block ml-2">
+            <FontAwesomeIcon icon={faCaretDown} />
+          </span>
+        </button>
+        <ul
+          id="pages"
+          className={togglePages ? 'w-auto ml-5 border-4 border-light-blue-500' : 'hidden'}
+        >
           {
               pages.map((page) => (
-                <li key={page} value={page}>
-                  <NavLink to="#">
+                <li key={page}>
+                  <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
                     <span>{page}</span>
                   </NavLink>
                 </li>
@@ -29,20 +47,29 @@ const NavMenu = () => {
         </ul>
       </div>
 
-      <NavLink to="#" className="inline-block">
+      <NavLink to="#" className="inline-block" onClick={() => { handleToggleNav(); resetToggles(); }}>
         <span>Testimonial</span>
       </NavLink>
 
-      <div className=" w-full h-auto">
-        <span>Featured Cars</span>
-        <span className="inline-block ml-2">
-          <FontAwesomeIcon icon={faCaretDown} />
-        </span>
-        <ul id="featured-cars" className="ml-2 hidden">
+      <div className=" w-full h-auto relative">
+        <button
+          className="inline"
+          type="button"
+          onClick={() => { setToggleFeaturedCars((prev) => !prev); }}
+        >
+          <span>Featured Cars</span>
+          <span className="inline-block ml-2">
+            <FontAwesomeIcon icon={faCaretDown} />
+          </span>
+        </button>
+        <ul
+          id="featured-cars"
+          className={toggleFeaturedCars ? 'border-4 border-light-blue-500 ml-5' : 'hidden'}
+        >
           {
               featuredCars.map((category) => (
-                <li key={category} value={category}>
-                  <NavLink to="#">
+                <li key={category}>
+                  <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
                     <span>{category}</span>
                   </NavLink>
                 </li>
@@ -51,23 +78,68 @@ const NavMenu = () => {
         </ul>
       </div>
 
-      <NavLink to="#" className="inline-block">
+      <NavLink to="#" className="inline-block" onClick={() => { handleToggleNav(); resetToggles(); }}>
         <span>More</span>
       </NavLink>
 
-      <NavLink to="#" className="inline-block">
+      <NavLink to="#" className="inline-block" onClick={() => { handleToggleNav(); resetToggles(); }}>
         <span>Contact</span>
       </NavLink>
 
-      <NavLink to="#" className="inline-block">
-        <span>Testimonial</span>
-      </NavLink>
+      <div className=" w-full h-auto">
+        <button className="inline" type="button" onClick={() => setToggleProfile((prev) => !prev)}>
+          <span>Profile</span>
+          <span className="inline-block ml-2">
+            <FontAwesomeIcon icon={faCaretDown} />
+          </span>
+        </button>
+        <ul
+          id="user-features"
+          className={toggleProfile ? 'ml-5 border-4 border-light-blue-500' : 'hidden'}
+        >
+          {
+            loggedIn
+            && (
+            <>
+              <li>
+                <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
+                  <span>Login</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
+                  <span>Signup</span>
+                </NavLink>
+              </li>
+            </>
+            )
 
-      <NavLink to="#" className="inline-block">
-        <span>Profile</span>
-      </NavLink>
+          }
+          {
+            !loggedIn && (
+            <>
+              <li>
+                <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
+                  <span>Profile</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#" onClick={() => { handleToggleNav(); resetToggles(); }}>
+                  <span>Logout</span>
+                </NavLink>
+              </li>
+            </>
+            )
+          }
+        </ul>
+      </div>
 
     </div>
   );
+};
+
+NavMenu.propTypes = {
+  toggle: PropTypes.bool.isRequired,
+  handleToggleNav: PropTypes.func.isRequired,
 };
 export default NavMenu;
